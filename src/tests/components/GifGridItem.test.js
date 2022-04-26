3547ed3c26df12f05094544fee1c51021bf513f1
emanuelhardwell <emanuelnoe16@gmail.com>
2022-04-26 10:34:18 -0500

    prueba en el componente GifGridItem de tipo includes

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -9,9 +9,45 @@ import { GifGridItem } from "../../components/GifGridItem";
 describe("Prueba en <GifGridItem />", () => {
   const title = "title de la imagen";
   const url = "http://localhost/url-de-la-imagen.jpg";
+  const claseCss = "card-img-top";
+
+  // Se puede poner el WRAPPER de forma global para que se pueda reutilizar en todos los TEST que pertenesca a este
+  const wrapper = shallow(<GifGridItem title={title} url={url} />);
 
   test("should mostrar el componente correctamente", () => {
-    const wrapper = shallow(<GifGridItem title={title} url={url} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test("Deberia mostrar el parrafo P igual", () => {
+    const res = wrapper.find("p");
+    expect(res.text().trim()).toBe(title);
+  });
+
+  test("Deberia tener los PROPS de SRC y ALT", () => {
+    const imagen = wrapper.find("img");
+
+    console.log(imagen.html());
+
+    console.log(imagen.prop("src"));
+
+    expect(imagen.prop("src")).toBe(url);
+    expect(imagen.prop("alt")).toBe(title);
+  });
+
+  test("Deberia tener los PROPS de CLASSNAME", () => {
+    const imagen = wrapper.find("img");
+
+    console.log(imagen.prop("className"));
+
+    expect(imagen.prop("className")).toBe(claseCss);
+  });
+
+  test("Deberia tener los PROPS de CLASSNAME ---> forma 2", () => {
+    const imagen = wrapper.find("img");
+
+    const className = imagen.prop("className");
+    console.log(className);
+
+    expect(className.includes("card-img-top")).toBe(true);
   });
 });
