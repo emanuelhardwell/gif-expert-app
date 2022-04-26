8d33d5aabd4bb144dafca6d06fe3ca9c1b42380a
emanuelhardwell <emanuelnoe16@gmail.com>
2022-04-26 17:19:28 -0500

    Pruebas usando mockReturnValue para simular la DATA que nos devuelve un CustomHook

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { useFetchGifs } from "../hooks/useFetchGifs";
 // import { getGifs } from "../helpers/getGifs";
 import { GifGridItem } from "./GifGridItem";
@@ -48,4 +49,8 @@ export const GifGrid = ({ category }) => {
       </div>
     </>
   );
+};
+
+GifGrid.propTypes = {
+  category: PropTypes.string.isRequired,
 };
---
+++ src/tests/components/GifGrid.test.js
@@ -0,0 +1,35 @@
+import "@testing-library/jest-dom";
+import { shallow } from "enzyme";
+import { GifGrid } from "../../components/GifGrid";
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+
+jest.mock("../../hooks/useFetchGifs");
+
+//
+describe("Pruebas en el componente GifGrid.js", () => {
+  const category = "One Punch";
+
+  test("should mostrar el snapshot correctamente", () => {
+    useFetchGifs.mockReturnValue({
+      data: [],
+      loading: true,
+    });
+
+    const wrapper = shallow(<GifGrid category={category} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("should mostrar el snapshot correctamente, ya con una CATEGORY", () => {
+    const gifs = [
+      { id: "abc", title: "paco el chato", url: "http://example.com/paco.jpg" },
+    ];
+
+    useFetchGifs.mockReturnValue({
+      data: gifs,
+      loading: false,
+    });
+
+    const wrapper = shallow(<GifGrid category={category} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
